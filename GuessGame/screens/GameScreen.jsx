a65f48c59d58abe37552edfd7e1811f6e63b69b6
Nayan Mahera <maheranayan18@gmail.com>
2024-08-24 15:51:20 +0530

    make responsive through diff screen size

--- GuessGame/screens/GameScreen.jsx
+++ GuessGame/screens/GameScreen.jsx
@@ -1,4 +1,12 @@
-import { View, Text, Alert, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  Alert,
+  FlatList,
+  Dimensions,
+  useWindowDimensions,
+  ScrollView,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import Title from "../app/components/Title";
 import NumberContainer from "../app/components/NumberContainer";
@@ -7,7 +15,6 @@ import GameLogItem from "../app/GameLogItem";
 
 function generateRandomBetween(min, max, exclude) {
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
-
   if (rndNum === exclude) {
     return generateRandomBetween(min, max, exclude);
   } else {
@@ -19,6 +26,9 @@ let minBoundary = 1;
 let maxBoundary = 100;
 
 export default function GameScreen({ userNumber, onGameOver }) {
+  // getting width using Dimensions
+  const deviceWidth = Dimensions.get("window").width;
+  const { height, width } = useWindowDimensions();
   const initialGuess = generateRandomBetween(1, 100, userNumber);
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
   const [guessRound, setGuessRound] = useState([initialGuess]);
@@ -63,38 +73,49 @@ export default function GameScreen({ userNumber, onGameOver }) {
   const guessRoundListLength = guessRound.length;
 
   return (
-    <View className="mt-10 ">
-      <Title>Opponent's Guess</Title>
-      <NumberContainer>{currentGuess}</NumberContainer>
-      {/* Guess */}
-      <View>
-        <Text className="text-3xl text-center">Higher Or Lower ?</Text>
-        <View className="flex flex-row gap-4 justify-center mt-4">
-          <View>
-            <PrimaryButton
-              title="-"
-              onPress={nextGuessHandler.bind(this, "lower")}
-            />
+    <>
+      <ScrollView>
+        {height < 420 && <Title>Opponent's Guess</Title>}
+        <View
+          className={`mt-10 ${
+            height < 420 ? "flex-row items-center justify-center" : " flex"
+          }`}
+        >
+          {height > 420 && <Title>Opponent's Guess</Title>}
+          <NumberContainer>{currentGuess}</NumberContainer>
+          {/* Guess */}
+          <View className="bg-pink-900 py-4 rounded-3xl">
+            <Text className="text-3xl text-center text-orange-400 font-[Poppins]">
+              Higher Or Lower ?
+            </Text>
+            <View className="flex flex-row gap-4 justify-center mt-4">
+              <View>
+                <PrimaryButton
+                  title="-"
+                  onPress={nextGuessHandler.bind(this, "lower")}
+                />
+              </View>
+              <View>
+                <PrimaryButton
+                  title="+"
+                  onPress={nextGuessHandler.bind(this, "greater")}
+                />
+              </View>
+            </View>
           </View>
-          <View>
-            <PrimaryButton
-              title="+"
-              onPress={nextGuessHandler.bind(this, "greater")}
-            />
-          </View>
+          {/* <View>Log Rounds</View> */}
         </View>
-      </View>
-      <FlatList
-        data={guessRound}
-        keyExtractor={(item) => item}
-        renderItem={(itemData) => (
-          <GameLogItem
-            guess={itemData.item}
-            roundNumber={guessRoundListLength - itemData.index}
-          />
-        )}
-      />
-      {/* <View>Log Rounds</View> */}
-    </View>
+        <FlatList
+          data={guessRound}
+          keyExtractor={(item) => item}
+          renderItem={(itemData) => (
+            <GameLogItem
+              guess={itemData.item}
+              roundNumber={guessRoundListLength - itemData.index}
+            />
+          )}
+        />
+      </ScrollView>
+    </>
   );
 }
--- GuessGame/screens/StartGameScreen.jsx
+++ GuessGame/screens/StartGameScreen.jsx
@@ -1,9 +1,21 @@
-import { View, TextInput, Text, Alert, StyleSheet } from "react-native";
-import React, { useState } from "react";
+import {
+  View,
+  TextInput,
+  Text,
+  Alert,
+  StyleSheet,
+  useWindowDimensions,
+  KeyboardAvoidingView,
+  ScrollView,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import PrimaryButton from "../app/components/PrimaryButton";
 import Title from "../app/components/Title";
 
 export default function StartGameScreen({ onPickNumber }) {
+  const { width, height } = useWindowDimensions();
+  console.log(width, height);
+
   const [enteredNumber, setEnteredNumber] = useState("");
   const numberInputHandler = (enteredText) => {
     setEnteredNumber(enteredText);
@@ -22,34 +34,39 @@ export default function StartGameScreen({ onPickNumber }) {
 
     onPickNumber(choosenNumber);
   };
-  return (
-    <View className="mt-24 ">
-      <Title>Guess Number</Title>
 
-      <View
-        style={{ elevation: 20 }}
-        className="flex justify-center items-center mx-6 rounded-lg p-4 mt-20 bg-pink-800"
-      >
-        <Text className="text-orange-400 text-xl">Enter Your Guess</Text>
-        <TextInput
-          value={enteredNumber}
-          onChangeText={numberInputHandler}
-          maxLength={2}
-          className=" bg-pink-800 text-white  p-2 mb-4 text-2xl h-12 w-16 border-[#ddb52f] border-b-4 px-4  text-center font-bold"
-          keyboardType="number-pad"
-          autoCorrect={false}
-          autoCapitalize="none"
-        />
-        <View className="flex flex-row  gap-4">
-          <View className="flex-1">
-            <PrimaryButton onPress={resetInputHandler} title="Reset" />
-          </View>
-          <View className="flex-1">
-            <PrimaryButton onPress={confirmInputHandler} title="Confirm" />
+  return (
+    <ScrollView>
+      <KeyboardAvoidingView className="flex-1" behavior="position">
+        <View className={`${height < 420 ? "mt-4" : "mt-24"}`}>
+          <Title>Guess Number</Title>
+
+          <View
+            style={{ elevation: 20 }}
+            className="flex justify-center items-center mx-6 rounded-lg p-4 mt-20 bg-pink-800"
+          >
+            <Text className="text-orange-400 text-xl">Enter Your Guess</Text>
+            <TextInput
+              value={enteredNumber}
+              onChangeText={numberInputHandler}
+              maxLength={2}
+              className=" bg-pink-800 text-white  p-2 mb-4 text-2xl h-12 w-16 border-[#ddb52f] border-b-4 px-4  text-center font-bold"
+              keyboardType="number-pad"
+              autoCorrect={false}
+              autoCapitalize="none"
+            />
+            <View className="flex flex-row  gap-4">
+              <View className="flex-1">
+                <PrimaryButton onPress={resetInputHandler} title="Reset" />
+              </View>
+              <View className="flex-1">
+                <PrimaryButton onPress={confirmInputHandler} title="Confirm" />
+              </View>
+            </View>
           </View>
         </View>
-      </View>
-    </View>
+      </KeyboardAvoidingView>
+    </ScrollView>
   );
 }
 
